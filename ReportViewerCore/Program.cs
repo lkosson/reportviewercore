19d4e8ff5914a68623ea032c7e0d421a8263ce5a
Łukasz Kosson <lukasz@kosson.net>
2020-06-18 10:35:31 +0200

    Added test project

---
+++ ReportViewerCore/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.IO;
+
+namespace ReportViewerCore
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var items = new[] { new ReportItem { Description = "Widget 6000", Price = 104.99m, Qty = 1 }, new ReportItem { Description = "Gizmo MAX", Price = 1.41m, Qty = 25 } };
+			var parameters = new[] { new ReportParameter("Title", "Invoice 4/2020") };
+			using var fs = new FileStream("Report.rdlc", FileMode.Open);
+			var report = new LocalReport();
+			report.LoadReportDefinition(fs);
+			report.DataSources.Add(new ReportDataSource("Items", items));
+			report.SetParameters(parameters);
+			var pdf = report.Render("PDF");
+			File.WriteAllBytes("report.pdf", pdf);
+		}
+	}
+}
