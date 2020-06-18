46e792c6be99e8b1c56c3036d5b5e9906cf5cfc7
Łukasz Kosson <lukasz@kosson.net>
2020-06-18 10:02:39 +0200

    Added compatibility assembly

---
+++ Microsoft.ReportViewer.NETCore/Compatibility/StrongNameIdentityPermissionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Text;
+
+namespace Microsoft.ReportingServices
+{
+	[AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+	public class StrongNameIdentityPermissionAttribute : Attribute
+	{
+		public string PublicKey { get; set; }
+
+		public StrongNameIdentityPermissionAttribute(SecurityAction securityAction)
+		{
+		}
+	}
+}
