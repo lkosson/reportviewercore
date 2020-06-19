50b3f33a1f8511879e4a45261fe8788e0dfe7e72
Łukasz Kosson <lukasz@kosson.net>
2020-06-19 13:14:32 +0200

    Added WinForms report preview

--- ReportViewerCore/Program.cs
+++ ReportViewerCore/Program.cs
@@ -8,15 +8,13 @@ namespace ReportViewerCore
 	{
 		static void Main(string[] args)
 		{
-			var items = new[] { new ReportItem { Description = "Widget 6000", Price = 104.99m, Qty = 1 }, new ReportItem { Description = "Gizmo MAX", Price = 1.41m, Qty = 25 } };
-			var parameters = new[] { new ReportParameter("Title", "Invoice 4/2020") };
-			using var fs = new FileStream("Report.rdlc", FileMode.Open);
-			var report = new LocalReport();
-			report.LoadReportDefinition(fs);
-			report.DataSources.Add(new ReportDataSource("Items", items));
-			report.SetParameters(parameters);
-			var pdf = report.Render("PDF");
-			File.WriteAllBytes("report.pdf", pdf);
+			using var form = new ReportViewerForm();
+			form.ShowDialog();
+
+			//var report = new LocalReport();
+			//Report.Load(report);
+			//var pdf = report.Render("PDF");
+			//File.WriteAllBytes("report.pdf", pdf);
 		}
 	}
 }
---
+++ ReportViewerCore/ReportViewerForm.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportViewerCore
+{
+	class ReportViewerForm : Form
+	{
+		private readonly ReportViewer reportViewer;
+
+		public ReportViewerForm()
+		{
+			Text = "Report viewer";
+			WindowState = FormWindowState.Maximized;
+			reportViewer = new ReportViewer();
+			reportViewer.Dock = DockStyle.Fill;
+			Controls.Add(reportViewer);
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			Report.Load(reportViewer.LocalReport);
+			reportViewer.RefreshReport();
+			base.OnLoad(e);
+		}
+	}
+}
