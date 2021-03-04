1bb46bcd05f90058d44e1ccdc72919ce32b4df94
Łukasz Kosson <git@lukasz.kosson.net>
2021-03-04 12:22:43 +0100

    Server-side WinForms ReportViewer sample application

---
+++ ReportViewerCore.Sample.WinFormsServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.IO;
+
+namespace ReportViewerCore
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			using var form = new ReportViewerForm();
+			form.ShowDialog();
+		}
+	}
+}
---
+++ ReportViewerCore.Sample.WinFormsServer/ReportViewerForm.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReportViewerCore
+{
+	class ReportViewerForm : Form
+	{
+		private readonly ReportViewer reportViewer;
+
+		public ReportViewerForm()
+		{
+			Text = "Report viewer";
+			WindowState = FormWindowState.Maximized;
+			reportViewer = new ReportViewer();
+			reportViewer.Dock = DockStyle.Fill;
+			Controls.Add(reportViewer);
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			reportViewer.ServerReport.ReportServerUrl = new Uri("http://localhost/ReportServer");
+			reportViewer.ServerReport.ReportPath = "Invoice";
+			reportViewer.ServerReport.SetParameters(new[] { new ReportParameter("Title", "Invoice 4/2020") });
+			reportViewer.RefreshReport();
+			base.OnLoad(e);
+		}
+	}
+}
