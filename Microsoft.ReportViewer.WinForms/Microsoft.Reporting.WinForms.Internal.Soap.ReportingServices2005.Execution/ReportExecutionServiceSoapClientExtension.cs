e18127366d184d06c98a3e4403fc007c6d4ec62b
Łukasz Kosson <git@lukasz.kosson.net>
2021-03-04 13:49:07 +0100

    Added webservice method adapters

--- Microsoft.ReportViewer.WinForms/Microsoft.Reporting.WinForms.Internal.Soap.ReportingServices2005.Execution/ReportExecutionServiceSoapClientExtension.cs
+++ Microsoft.ReportViewer.WinForms/Microsoft.Reporting.WinForms.Internal.Soap.ReportingServices2005.Execution/ReportExecutionServiceSoapClientExtension.cs
@@ -44,172 +44,212 @@ namespace Microsoft.Reporting.WinForms.Internal.Soap.ReportingServices2005.Execu
 
 		public ExecutionInfo LoadReport(string Report, string HistoryID)
 		{
-			throw new NotImplementedException();
+			LoadReport(TrustedUserHeaderValue, Report, HistoryID, out var serverInfoHeader, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo LoadReport2(string Report, string HistoryID)
 		{
-			throw new NotImplementedException();
+			LoadReport2(TrustedUserHeaderValue, Report, HistoryID, out var serverInfoHeader, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo LoadReport3(string Report, string HistoryID)
 		{
-			throw new NotImplementedException();
+			LoadReport3(TrustedUserHeaderValue, Report, HistoryID, out var serverInfoHeader, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo LoadReportDefinition(byte[] Definition, out Warning[] warnings)
 		{
-			throw new NotImplementedException();
+			LoadReportDefinition(TrustedUserHeaderValue, Definition, out var serverInfoHeader, out var executionInfo, out warnings);
+			return executionInfo;
 		}
 
 		public ExecutionInfo LoadReportDefinition2(byte[] Definition, out Warning[] warnings)
 		{
-			throw new NotImplementedException();
+			LoadReportDefinition2(TrustedUserHeaderValue, Definition, out var serverInfoHeader, out var executionInfo, out warnings);
+			return executionInfo;
 		}
 
 		public ExecutionInfo LoadReportDefinition3(byte[] Definition, out Warning[] warnings)
 		{
-			throw new NotImplementedException();
+			LoadReportDefinition3(TrustedUserHeaderValue, Definition, out var serverInfoHeader, out var executionInfo, out warnings);
+			return executionInfo;
 		}
 
 		public ExecutionInfo SetExecutionCredentials(DataSourceCredentials[] Credentials)
 		{
-			throw new NotImplementedException();
+			SetExecutionCredentials(ExecutionHeaderValue, TrustedUserHeaderValue, Credentials, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo SetExecutionCredentials2(DataSourceCredentials[] Credentials)
 		{
-			throw new NotImplementedException();
+			SetExecutionCredentials2(ExecutionHeaderValue, TrustedUserHeaderValue, Credentials, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo SetExecutionCredentials3(DataSourceCredentials[] Credentials)
 		{
-			throw new NotImplementedException();
+			SetExecutionCredentials3(ExecutionHeaderValue, TrustedUserHeaderValue, Credentials, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo SetExecutionParameters(ParameterValue[] Parameters, string ParameterLanguage)
 		{
-			throw new NotImplementedException();
+			SetExecutionParameters(ExecutionHeaderValue, TrustedUserHeaderValue, Parameters, ParameterLanguage, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo SetExecutionParameters2(ParameterValue[] Parameters, string ParameterLanguage)
 		{
-			throw new NotImplementedException();
+			SetExecutionParameters2(ExecutionHeaderValue, TrustedUserHeaderValue, Parameters, ParameterLanguage, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo SetExecutionParameters3(ParameterValue[] Parameters, string ParameterLanguage)
 		{
-			throw new NotImplementedException();
+			SetExecutionParameters3(ExecutionHeaderValue, TrustedUserHeaderValue, Parameters, ParameterLanguage, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo ResetExecution()
 		{
-			throw new NotImplementedException();
+			ResetExecution(ExecutionHeaderValue, TrustedUserHeaderValue, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo ResetExecution2()
 		{
-			throw new NotImplementedException();
+			ResetExecution2(ExecutionHeaderValue, TrustedUserHeaderValue, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo ResetExecution3()
 		{
-			throw new NotImplementedException();
+			ResetExecution3(ExecutionHeaderValue, TrustedUserHeaderValue, out var executionInfo);
+			return executionInfo;
+
 		}
 
 		public byte[] Render(string Format, string DeviceInfo, out string Extension, out string MimeType, out string Encoding, out Warning[] Warnings, out string[] StreamIds)
 		{
-			throw new NotImplementedException();
+			Render(ExecutionHeaderValue, TrustedUserHeaderValue, Format, DeviceInfo, out var result, out Extension, out MimeType, out Encoding, out Warnings, out StreamIds);
+			return result;
 		}
 
 		public byte[] Render2(string Format, string DeviceInfo, PageCountMode PaginationMode, out string Extension, out string MimeType, out string Encoding, out Warning[] Warnings, out string[] StreamIds)
 		{
-			throw new NotImplementedException();
+			Render2(ExecutionHeaderValue, TrustedUserHeaderValue, Format, DeviceInfo, PaginationMode, out var result, out Extension, out MimeType, out Encoding, out Warnings, out StreamIds);
+			return result;
 		}
 
 		public byte[] RenderStream(string Format, string StreamID, string DeviceInfo, out string Encoding, out string MimeType)
 		{
-			throw new NotImplementedException();
+			RenderStream(ExecutionHeaderValue, TrustedUserHeaderValue, Format, StreamID, DeviceInfo, out var result, out Encoding, out MimeType);
+			return result;
 		}
 
 		public void DeliverReportItem(string Format, string DeviceInfo, ExtensionSettings ExtensionSettings, string Description, string EventType, string MatchData)
 		{
-			throw new NotImplementedException();
+			DeliverReportItem(ExecutionHeaderValue, TrustedUserHeaderValue, Format, DeviceInfo, ExtensionSettings, Description, EventType, MatchData);
 		}
 
 		public ExecutionInfo GetExecutionInfo()
 		{
-			throw new NotImplementedException();
+			GetExecutionInfo(ExecutionHeaderValue, TrustedUserHeaderValue, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo GetExecutionInfo2()
 		{
-			throw new NotImplementedException();
+			GetExecutionInfo2(ExecutionHeaderValue, TrustedUserHeaderValue, out var executionInfo);
+			return executionInfo;
 		}
 
 		public ExecutionInfo GetExecutionInfo3()
 		{
-			throw new NotImplementedException();
+			GetExecutionInfo3(ExecutionHeaderValue, TrustedUserHeaderValue, out var executionInfo);
+			return executionInfo;
 		}
 
 		public DocumentMapNode GetDocumentMap()
 		{
-			throw new NotImplementedException();
+			GetDocumentMap(ExecutionHeaderValue, TrustedUserHeaderValue, out var documentMapNode);
+			return documentMapNode;
 		}
 
 		public ExecutionInfo LoadDrillthroughTarget(string DrillthroughID)
 		{
-			throw new NotImplementedException();
+			var executionHeader = ExecutionHeaderValue;
+			LoadDrillthroughTarget(ref executionHeader, TrustedUserHeaderValue, DrillthroughID, out var executionInfo);
+			ExecutionHeaderValue = executionHeader;
+			return executionInfo;
 		}
 
 		public ExecutionInfo LoadDrillthroughTarget2(string DrillthroughID)
 		{
-			throw new NotImplementedException();
+			var executionHeader = ExecutionHeaderValue;
+			LoadDrillthroughTarget2(ref executionHeader, TrustedUserHeaderValue, DrillthroughID, out var executionInfo);
+			ExecutionHeaderValue = executionHeader;
+			return executionInfo;
 		}
 
 		public ExecutionInfo LoadDrillthroughTarget3(string DrillthroughID)
 		{
-			throw new NotImplementedException();
+			var executionHeader = ExecutionHeaderValue;
+			LoadDrillthroughTarget3(ref executionHeader, TrustedUserHeaderValue, DrillthroughID, out var executionInfo);
+			ExecutionHeaderValue = executionHeader;
+			return executionInfo;
 		}
 
 		public bool ToggleItem(string ToggleID)
 		{
-			throw new NotImplementedException();
+			ToggleItem(ExecutionHeaderValue, TrustedUserHeaderValue, ToggleID, out var found);
+			return found;
 		}
 
 		public int NavigateDocumentMap(string DocMapID)
 		{
-			throw new NotImplementedException();
+			NavigateDocumentMap(ExecutionHeaderValue, TrustedUserHeaderValue, DocMapID, out var pageNumber);
+			return pageNumber;
 		}
 
 		public int NavigateBookmark(string BookmarkID, out string UniqueName)
 		{
-			throw new NotImplementedException();
+			NavigateBookmark(ExecutionHeaderValue, TrustedUserHeaderValue, BookmarkID, out var pageNumber, out UniqueName);
+			return pageNumber;
 		}
 
 		public int Sort(string SortItem, SortDirectionEnum Direction, bool Clear, out string ReportItem, out int NumPages)
 		{
-			throw new NotImplementedException();
+			Sort(ExecutionHeaderValue, TrustedUserHeaderValue, SortItem, Direction, Clear, out var pageNumber, out ReportItem, out NumPages);
+			return pageNumber;
 		}
 
 		public int Sort2(string SortItem, SortDirectionEnum Direction, bool Clear, PageCountMode PaginationMode, out string ReportItem, out ExecutionInfo2 ExecutionInfo)
 		{
-			throw new NotImplementedException();
+			Sort2(ExecutionHeaderValue, TrustedUserHeaderValue, SortItem, Direction, Clear, PaginationMode, out var pageNumber, out ReportItem, out ExecutionInfo);
+			return pageNumber;
 		}
 
 		public int FindString(int startPage, int endPage, string findValue)
 		{
-			throw new NotImplementedException();
+			FindString(ExecutionHeaderValue, TrustedUserHeaderValue, startPage, endPage, findValue, out var pageNumber);
+			return pageNumber;
 		}
 
 		public byte[] GetRenderResource(string Format, string DeviceInfo, out string MimeType)
 		{
-			throw new NotImplementedException();
+			GetRenderResource(TrustedUserHeaderValue, Format, DeviceInfo, out var result, out MimeType);
+			return result;
 		}
 
 		public Extension[] ListRenderingExtensions()
 		{
-			throw new NotImplementedException();
+			ListRenderingExtensions(TrustedUserHeaderValue, out var extensions);
+			return extensions;
 		}
 	}
 }
