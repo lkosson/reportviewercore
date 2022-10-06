46d824606f238547ac01de78eda1b8b5629a1bb0
Łukasz Kosson <git@lukasz.kosson.net>
2022-10-06 13:12:20 +0200

    SPB PDF experiments

--- Microsoft.ReportViewer.Common/Extensions.ITextSharpRendering/ITextSharpRenderer.cs
+++ Microsoft.ReportViewer.Common/Extensions.ITextSharpRendering/ITextSharpRenderer.cs
@@ -91,15 +91,30 @@ namespace Extensions.ITextSharpRendering
 		public bool Render(Microsoft.ReportingServices.OnDemandReportRendering.Report report, NameValueCollection reportServerParameters, NameValueCollection deviceInfo, NameValueCollection clientCapabilities, ref Hashtable renderProperties, CreateAndRegisterStream createAndRegisterStream)
 		{
 			var stream = createAndRegisterStream(report.Name, "pdf", null, "application/pdf", false, StreamOper.CreateAndRegister);
-			using var renderer = new PDFRenderer();
-			using var writer = new PDFWriter(renderer, stream, false, createAndRegisterStream);
-			using var hpbProcessing = new HPBProcessing(report, deviceInfo, createAndRegisterStream, ref renderProperties);
-			hpbProcessing.SetContext(hpbProcessing.PaginationSettings.StartPage, hpbProcessing.PaginationSettings.EndPage);
-			writer.BeginReport(hpbProcessing.PaginationSettings.DpiX, hpbProcessing.PaginationSettings.DpiY);
+			var renderer = new Microsoft.ReportingServices.Rendering.ImageRenderer.Renderer(true, new Microsoft.ReportingServices.Rendering.RichText.FontCache(200));
+			using var writer = new Microsoft.ReportingServices.Rendering.ImageRenderer.PDFWriter(renderer, stream, false, createAndRegisterStream, 200, 200);
+			using var spbProcessing = new SPBProcessing(report, createAndRegisterStream, false, ref renderProperties);
+
+			var spbContext = new SPBContext();
+			spbContext.StartPage = 0;
+			spbContext.EndPage = 0;
+			spbContext.MeasureItems = false;
+			spbContext.AddSecondaryStreamNames = true;
+			spbContext.AddToggledItems = true;
+			spbContext.AddOriginalValue = true;
+			spbProcessing.SetContext(spbContext);
+
+			writer.BeginReport(200, 200);
+			//using var renderer = new PDFRenderer();
+			//using var writer = new PDFWriter(renderer, stream, false, createAndRegisterStream);
+			//using var hpbProcessing = new HPBProcessing(report, deviceInfo, createAndRegisterStream, ref renderProperties);
+			//hpbProcessing.SetContext(hpbProcessing.PaginationSettings.StartPage, hpbProcessing.PaginationSettings.EndPage);
+			//writer.BeginReport(hpbProcessing.PaginationSettings.DpiX, hpbProcessing.PaginationSettings.DpiY);
 			int num = 0;
 			while (true)
 			{
-				hpbProcessing.GetNextPage(out RPLReport rplReport);
+				//hpbProcessing.GetNextPage(out RPLReport rplReport);
+				spbProcessing.GetNextPage(out RPLReport rplReport);
 				if (rplReport == null) break;
 				renderer.ProcessPage(rplReport, num, null, null);
 				rplReport.Release();
